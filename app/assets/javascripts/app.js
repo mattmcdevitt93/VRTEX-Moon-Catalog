e51493fc86707ebc04c0817f0867d5ab334b7dc2
Mattmcdevitt93 <Mattmcdevitt93@gmail.com>
2017-10-24 08:37:02 -0400

    precompile fix

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,53 +9,6 @@ $(document).ready(function() {
 
 Module.bindings = function () {
 
-	// $("#moon_Region_id").change(function() {
-	// 	console.log( "Update Constelation: " + $("#moon_Region_id").val() );
-	// 	Module.getConsetllationsByID($("#moon_Region_id").val());
-	// 	Module.hideSelectFields();
-	// });
-
-	// $("#moon_Constellation_id").change(function() {
-	// 	console.log( "Update System: " + $("#moon_Constellation_id").val() );
-	// 	Module.getSystemByID($("#moon_Constellation_id").val());
-	// 	Module.hideSelectFields();
-	// });
-
-	// $("#moon_System_id").change(function() {
-	// 	console.log( "Update Planet: " + $("#moon_System_id").val() );
-	// 	Module.getPlanetByID($("#moon_System_id").val());
-	// 	Module.hideSelectFields();
-	// });
-
-	// $("#moon_Planet_id").change(function() {
-	// 	console.log( "Update Planet: " + $("#moon_Planet_id").val() );
-	// 	Module.getMoonbyID($("#moon_System_id").val(), $("#moon_Planet_id").find('option:selected').attr("name") );
-	// 	Module.hideSelectFields();
-	// });
-
-	// $("#moon_Moon_id").change(function() {
-	// 	Module.hideSelectFields();
-	// });
-
-	// for (var i = 0; i <= 4; i++) {
-	// 	var handle = $( ".handle_" + i );
-	// 	var slider = $( "#" + i );
-	// 	Module.sliderBindings(slider, handle);
-
-	// }
-
-
-	// $( ".slider" ).slider( {
-	// 	create: function() {
-	// 		handle.text( $( this ).slider( "value" ) );
-	// 	},
-	// 	slide: function( event, ui ) {
-	// 		handle.text( ui.value );
-	// 		console.log( ui.value )
-	// 		// Module.contentUpdate(ui.value, 0);
-	// 	}
-	// } );
-
 }
 
 // Module.sliderBindings = function (slider_id, handle) {
@@ -191,5 +144,5 @@ Module.bindings = function () {
 // 		}
 // 	});
 	// ["Derelik", "The Forge", "Vale of the Silent", "Wicked Creek", "Cache", "Detorid", "Scalding Pass", "Insmother", "UUA-F4", "Great Wildlands", "Tribute", "The Spire", "Malpais", "Lonetrek", "Venal", "Catch", "Curse", "Domain", "Immensea", "The Bleak Lands", "Geminate", "Paragon Soul", "Tenal", "Solitude", "Stain", "Tenerifis", "Outer Passage", "Feythabolis", "Tash-Murkon", "Delve", "C-R00011", "Sinq Laison", "Pure Blind", "Kador", "Cobalt Edge", "Providence", "Aridia", "A821-A", "Cloud Ring", "Perrigen Falls", "C-R00013", "Fountain", "The Kalevala Expanse", "Molden Heath", "C-R00012", "Placid", "D-R00020", "Metropolis", "B-R00006", "B-R00007", "E-R00026", "The Citadel", "C-R00015", "D-R00018", "Khanid", "C-R00010", "D-R00022", "C-R00009", "H-R00032", "Deklein", "Verge Vendor", "Period Basis", "D-R00021", "E-R00029", "Etherium Reach", "D-R00023", "A-R00002", "B-R00004", "F-R00030", "Syndicate", "Esoteria", "B-R00005", "Kor-Azor", "K-R00033", "C-R00014", "Branch", "Heimatar", "Querious", "Essence", "Oasa", "A-R00003", "Genesis", "Fade", "E-R00024", "Impass", "D-R00016", "E-R00025", "A-R00001", "Outer Ring", "Omist", "E-R00027", "D-R00017", "B-R00008", "Devoid", "Black Rise", "G-R00031", "D-R00019", "Everyshore", "J7HZ-F", "E-R00028"]
-};
+// };
 
